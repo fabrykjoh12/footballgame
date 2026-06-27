9f4c768c558734b23a32ccad7a21791b033215be
fabrykjoh12 <fabrykjoh12@users.noreply.github.com>
2026-06-27 19:08:32 +0000

    deploy: 9bfefd15fe4f09ec31d5f9be8e3e859d4677814f

--- assets/ablyGameService-BOpmTFty.js
+++ assets/ablyGameService-BOpmTFty.js
@@ -1,4 +1,4 @@
-var ea=Object.defineProperty;var ta=(j,y,v)=>y in j?ea(j,y,{enumerable:!0,configurable:!0,writable:!0,value:v}):j[y]=v;var B=(j,y,v)=>ta(j,typeof y!="symbol"?y+"":y,v);import{a as na,g as sa,M as ia,n as ra,p as oa,r as aa,b as ca,d as la,u as ha}from"./index-DP-iGJDM.js";var ae=typeof globalThis<"u"?globalThis:typeof window<"u"?window:typeof global<"u"?global:typeof self<"u"?self:{},ei={exports:{}};/*@license Copyright 2015-2022 Ably Real-time Ltd (ably.com)
+var ea=Object.defineProperty;var ta=(j,y,v)=>y in j?ea(j,y,{enumerable:!0,configurable:!0,writable:!0,value:v}):j[y]=v;var B=(j,y,v)=>ta(j,typeof y!="symbol"?y+"":y,v);import{a as na,g as sa,M as ia,n as ra,p as oa,r as aa,b as ca,d as la,u as ha}from"./index-DnLrxX5d.js";var ae=typeof globalThis<"u"?globalThis:typeof window<"u"?window:typeof global<"u"?global:typeof self<"u"?self:{},ei={exports:{}};/*@license Copyright 2015-2022 Ably Real-time Ltd (ably.com)
 
 Ably JavaScript Library v2.23.0
 https://github.com/ably/ably-js
