8fe0e8706e6d05301fb336b2ddf7c375ff79971b
fabrykjoh12 <fabrykjoh12@users.noreply.github.com>
2026-07-04 19:04:10 +0000

    deploy: 939b388c940597d939f13ca722858616a6790e44

--- assets/firebaseBackend-wF45K5Au.js
+++ assets/firebaseBackend-wF45K5Au.js
@@ -1,4 +1,4 @@
-import{au as fc}from"./index-FfZoetn_.js";const Yd=()=>{};var pc={};/**
+import{au as fc}from"./index-CP7dGRmB.js";const Yd=()=>{};var pc={};/**
  * @license
  * Copyright 2017 Google LLC
  *
